914979a8091b26a7af0a6043f033ddd2c6fedec6
haydar-99 <haydaralsaadi33@gmail.com>
2021-06-09 14:38:07 +0200

    fixed move-method. keybinded the snake

--- SnakeProjekt[Mju-20]-Haydar-Anthony-Basma/Snake-abstract.cs
+++ SnakeProjekt[Mju-20]-Haydar-Anthony-Basma/Snake-abstract.cs
@@ -8,18 +8,16 @@ namespace SnakeProjekt_Mju_20__Haydar_Anthony_Basma
 {
     abstract class Snake_abstract
     {
-        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        public ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
         public char key = 'w';
         public char dir = 'u';
-        public Dictionary<int, int> bodyList;
+        public List<Cordinate> SnakeCordinates;
         public int X { get; set; }
         public int Y{ get; set; }
         public Snake_abstract()
         {
-            X = 20;
-            Y = 20;
-            bodyList = new Dictionary<int, int>();
-            bodyList.Add(X, Y);
+           
+           
         }
 
         public abstract void DrawSnake();
--- SnakeProjekt[Mju-20]-Haydar-Anthony-Basma/snakeObj.cs
+++ SnakeProjekt[Mju-20]-Haydar-Anthony-Basma/snakeObj.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SnakeProjekt_Mju_20__Haydar_Anthony_Basma
@@ -12,20 +13,70 @@ namespace SnakeProjekt_Mju_20__Haydar_Anthony_Basma
 
         public snakeObj()
         {
-            
+            X = 20;
+            Y = 5;
+            SnakeCordinates = new List<Cordinate>();
+            SnakeCordinates.Add(new Cordinate(X, Y));
         }
         public override void DrawSnake()
         {
-            foreach (KeyValuePair<int, int> pos in bodyList)
+            foreach (var  pos in SnakeCordinates)
             {
-                Console.SetCursorPosition(pos.Key, pos.Value);
+                Console.SetCursorPosition(pos.X, pos.Y);
                 Console.WriteLine("@");
             }
 
         }
+        private void direction()
+        {
+            if (key == 'w' && dir != 'd')
+            {
+                dir = 'u';
+            }
+            else if (key == 's' && dir != 'u')
+            {
+                dir = 'd';
+            }
+            else if (key == 'd' && dir != 'l')
+            {
+                dir = 'r';
+            }
+            else if (key == 'a' && dir != 'r')
+            {
+                dir = 'l';
+            }
+        }
         public override void MoveSnake()
         {
-            throw new NotImplementedException();
+            direction();
+            if (dir == 'u')
+            {
+                Y--;
+            }
+            else if (dir == 'd')
+            {
+                Y++;
+            }
+            else if (dir == 'r')
+            {
+                X++;
+            }
+            else if (dir == 'l')
+            {
+                X--;
+            }
+            SnakeCordinates.Add(new Cordinate(X, Y));
+            Thread.Sleep(300);
+            SnakeCordinates.RemoveAt(0);
+
+        }
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
         }
 
     }
