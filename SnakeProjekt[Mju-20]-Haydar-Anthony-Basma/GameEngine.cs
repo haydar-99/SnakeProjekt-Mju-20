d30f3fa8a9f56b031b0b379169f88694bcb467b6
haydar-99 <haydaralsaadi33@gmail.com>
2021-06-14 16:44:39 +0200

    fixed the gameEngine, refactored the main program

---
+++ SnakeProjekt[Mju-20]-Haydar-Anthony-Basma/GameEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeProjekt_Mju_20__Haydar_Anthony_Basma
+{
+    class GameEngine
+    {
+        public static MainMenu mainMenu = new MainMenu();
+        public static GameGround gameGround = new GameGround();
+        public static SnakeFood snakeFood = new SnakeFood();
+        public static snakeObj snake = new snakeObj();
+
+        public static void StartGame(MainMenu mainMenu, SnakeFood snakeFood, snakeObj snake)
+        {
+            while (true)
+            {
+                mainMenu.ViewMainMenu();
+                if (mainMenu.start == true)
+                {
+                    while (true)
+                    {
+                        GameGround.BuildGround();
+
+                        snake.Input();
+                        snakeFood.DrawFood();
+
+                        snake.DrawSnake();
+
+                        snake.MoveSnake();
+                        snake.SnakeGrow(snakeFood);
+                        if (snake.DeadSnake())
+                        {
+                            mainMenu.start = false;
+                            break;
+                        }
+                    }
+
+                }
+
+
+
+            }
+        }
+    }
+}
+
